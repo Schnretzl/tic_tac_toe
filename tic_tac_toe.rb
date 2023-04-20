3a07364a521d8483cf063139a6a4ebbc64c3363f
Wes Hendley <wrhendley@gmail.com>
2023-04-20 13:51:11 -0500

    Add ruby file with initial class declarations

---
+++ tic_tac_toe.rb
@@ -0,0 +1,29 @@
+class player
+    def initialize
+        @name = name
+        @symbol = symbol
+    end
+end
+
+class square
+    def initialize
+        @coordinate = coordinate
+        @played = false
+    end
+end
+
+class board
+    def initialize(players, squares)
+        @players = players
+        @squares = squares
+    end
+
+    def squares
+
+    end
+    
+    def player
+
+    end
+
+end
