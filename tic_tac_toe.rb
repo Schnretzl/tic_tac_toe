9c31ff5a2cdcf7c4753fe480ba539cf604f76b0f
Wes Hendley <wrhendley@gmail.com>
2023-04-24 12:55:31 -0500

    Add main, add function to check lines for winner

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -39,11 +39,31 @@ class Board
 
 end
 
+def check_line(board)
+  ['ABC', 'DEF', 'GHI', 'ADG', 'BEH', 'CFI', 'AEI', 'CEG'].each do |line|
+    state = board[line[0]]
+    next if state.nil?
+
+    return state if board[line[1]] == state && board[line[2]] == state
+  end
+
+  return nil
+end
 
 puts 'Name of player 1?'
 p1 = Player.new(1, gets.chomp, 'X')
 puts 'Name of player 2?'
 p2 = Player.new(2, gets.chomp, 'O')
 board = Board.new(p1, p2)
-
-p board.squares['A'].played_symbol
+current_player_symbol = "X"
+while(true)
+  puts "Where do you want to play?"
+  square = gets.chomp.upcase
+  board.squares[square].played_symbol = current_player_symbol
+  winner = check_line(board)
+  if !winner.nil?
+    puts "#{winner.name} wins!"
+    break
+  end
+  current_player_symbol == 'X'? current_player_symbol = 'O' : current_player_symbol = 'X'
+end
