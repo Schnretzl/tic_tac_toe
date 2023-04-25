ed67f146e13f92801ba8feca336705b4cab12d28
Wes Hendley <wrhendley@gmail.com>
2023-04-25 13:25:54 -0500

    Implement Player.play method

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,23 +1,23 @@
-class Player  
+class Square
+  attr_reader :coordinate
+  attr_accessor :played_symbol
+
+  def initialize(coordinate, played_symbol = nil)
+    @coordinate = coordinate
+    @played_symbol = nil
+  end
+end
+
+class Player
   def initialize(player_number, name = "", symbol)
-    @name = name.empty?? "Player #{player_number}" : name
+    @name = name.empty? ? "Player #{player_number}" : name
     @symbol = symbol
   end
 
   attr_reader :name, :symbol
 
-  def play(coordinate)
-
-  end
-end
-
-class Square
-  attr_reader :coordinate
-  attr_accessor :played_symbol
-  
-  def initialize(coordinate, played_symbol = nil)
-    @coordinate = coordinate
-    @played_symbol = nil
+  def play(coordinate, board)
+    board.squares[coordinate].played_symbol = symbol
   end
 end
 
@@ -32,11 +32,6 @@ class Board
   end
 
   attr_accessor :player1, :player2, :squares
-  
-  def player
-
-  end
-
 end
 
 def check_line(board)
@@ -50,17 +45,34 @@ def check_line(board)
   return nil
 end
 
+def display_board_moves(board)
+  a_symbol = board.squares['A'].played_symbol.nil? ? board.squares['A'].coordinate : ' '
+  b_symbol = board.squares['B'].played_symbol.nil? ? board.squares['B'].coordinate : ' '
+  c_symbol = board.squares['C'].played_symbol.nil? ? board.squares['C'].coordinate : ' '
+  puts "#{a_symbol.center(3)} | #{b_symbol.center(3)} | #{c_symbol.center(3)}"
+  puts '----+-----+----'
+  d_symbol = board.squares['D'].played_symbol.nil? ? board.squares['D'].coordinate : ' '
+  e_symbol = board.squares['E'].played_symbol.nil? ? board.squares['E'].coordinate : ' '
+  f_symbol = board.squares['F'].played_symbol.nil? ? board.squares['F'].coordinate : ' '
+  puts "#{d_symbol.center(3)} | #{e_symbol.center(3)} | #{f_symbol.center(3)}"
+  puts '----+-----+----'
+  g_symbol = board.squares['G'].played_symbol.nil? ? board.squares['G'].coordinate : ' '
+  h_symbol = board.squares['H'].played_symbol.nil? ? board.squares['H'].coordinate : ' '
+  i_symbol = board.squares['I'].played_symbol.nil? ? board.squares['I'].coordinate : ' '
+  puts "#{g_symbol.center(3)} | #{h_symbol.center(3)} | #{i_symbol.center(3)}"
+end
+
 def display_board(board)
   a_symbol = board.squares['A'].played_symbol || ''
   b_symbol = board.squares['B'].played_symbol || ''
   c_symbol = board.squares['C'].played_symbol || ''
   puts "#{a_symbol.center(3)} | #{b_symbol.center(3)} | #{c_symbol.center(3)}"
-  puts "----+----+----"
+  puts '----+-----+----'
   d_symbol = board.squares['D'].played_symbol || ''
   e_symbol = board.squares['E'].played_symbol || ''
   f_symbol = board.squares['F'].played_symbol || ''
   puts "#{d_symbol.center(3)} | #{e_symbol.center(3)} | #{f_symbol.center(3)}"
-  puts "----+---+----"
+  puts '----+-----+----'
   g_symbol = board.squares['G'].played_symbol || ''
   h_symbol = board.squares['H'].played_symbol || ''
   i_symbol = board.squares['I'].played_symbol || ''
@@ -75,12 +87,14 @@ board = Board.new(p1, p2)
 current_player = p1
 loop do
   puts "#{current_player.name}, where do you want to play?"
-  square = STDIN.gets.chomp.upcase
-  until /[A-I]/ =~ square && board.squares[square].played_symbol.nil?
-    puts "Please enter a valid square"
-    square = STDIN.gets.chomp.upcase
+  display_board_moves(board)
+  coordinate = STDIN.gets.chomp.upcase
+  until /[A-I]/ =~ coordinate && board.squares[coordinate].played_symbol.nil?
+    puts 'Please enter a valid square'
+    coordinate = STDIN.gets.chomp.upcase
   end
-  board.squares[square].played_symbol = current_player.symbol
+  # board.squares[coordinate].played_symbol = current_player.symbol
+  current_player.play(coordinate, board)
   winner = check_line(board)
   display_board(board)
   unless winner.nil?
