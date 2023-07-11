91989ef15169493ab83659a098b52938271dd367
Wes Hendley <wrhendley@gmail.com>
2023-07-11 16:04:32 -0500

    Add tests for row, column, and diagonal wins

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -11,4 +11,28 @@ describe '#check_line' do
   it 'Returns nil if there is no winner' do
     expect(check_line(game_board)).to be_nil
   end
+
+  it 'Returns \'X\' when there is an \'X\' across the top row' do
+    ['A', 'B', 'C'].each do |letter|
+      game_board.squares[letter].played_symbol = 'X'
+    end
+
+    expect(check_line(game_board)).to eq 'X'
+  end
+
+  it 'Returns \'O\' when there is an \'O\' down the middle column' do
+    ['B', 'E', 'H'].each do |letter|
+      game_board.squares[letter].played_symbol = 'O'
+    end
+
+    expect(check_line(game_board)).to eq 'O'
+  end
+
+  it 'Returns \'X\' when there is an \'X\' across the top left to bottom right diagonal' do
+    ['A', 'E', 'I'].each do |letter|
+      game_board.squares[letter].played_symbol = 'X'
+    end
+
+    expect(check_line(game_board)).to eq 'X'
+  end
 end
